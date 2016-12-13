1533a670a86c12b89e44389335d7a4720ebbd649
David Egan <dwe@eganrm.com>
2016-12-12 22:39:45 -0800

    Removed CSRF protection not needed no local inputs

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -79,8 +79,6 @@ $app->add(new \App\Middleware\ValidationErrorsMiddleware($container));
 $app->add(new \App\Middleware\OldInputMiddleware($container));
 $app->add(new \App\Middleware\CsrfViewMiddleware($container));
 
-//$app->add($container->csrf);
-
 
 v::with('App\\Validation\\Rules\\');
 
